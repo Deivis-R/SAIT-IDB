d584756e311cbf5bc3c78521b2f3ca8410416e22
Deividas Rasimas <deividas.rasimas@ktu.edu>
2024-11-21 22:02:31 +0200

    Register typo fix

--- shop/urls.py
+++ shop/urls.py
@@ -21,7 +21,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from .custom_claims import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterUser
+from api.views import RegisterUser
 
 schema_view = get_schema_view(
     openapi.Info(
